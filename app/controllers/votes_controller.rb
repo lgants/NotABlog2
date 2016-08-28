15494a5dd8f48fe23182a94eed273f116b7a62f4
colbyschulz <colbyschulz@gmail.com>
2016-08-28 14:31:10 -0400

    added vote gem

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,43 +1,14 @@
 class VotesController < ApplicationController
-  protect_from_forgery except: :create
-  # def new
-  #     @vote = Vote.new(vote_params)
-  # end
-
 
   def create
+    post = Post.find(params[:vote][:post_id])
 
-    if Vote.find_by(user_id: params[:vote][:user_id], post_id: params[:vote][:post_id])
-      old_vote = Vote.find_by(user_id: params[:vote][:user_id], post_id: params[:vote][:post_id])
-      old_vote.destroy
-      new_vote = Vote.create(user_id: params[:vote][:user_id], post_id: params[:vote][:post_id])
-      post = Post.find(params[:vote][:post_id])
-      post.votes << new_vote
-      post.save
-      redirect_to root_path, notice: "Vote updated!"
-    else
-      new_vote = Vote.create(user_id: params[:vote][:user_id], post_id: params[:vote][:post_id])
-      post = Post.find(params[:vote][:post_id])
-      post.votes << new_vote
-      post.save
-      redirect_to root_path, notice: "Vote created!"
+    if params[:vote][:vote_type] == "upvote"
+      post.upvote_by current_user
+      redirect_to root_path
+    elsif params[:vote][:vote_type] == "downvote"
+      post.downvote_by current_user
+      redirect_to root_path
     end
-
-
-    # if Vote.find_by(user_id: current_user.id, post_id: params[:vote][:post_id])
-    #   old_vote = Vote.find_by(user_id: current_user.id, post_id: params[:vote][:post_id])
-    #   old_vote.update(value: params[:vote][:updown])
-    #   redirect_to root_path, notice: "Vote updated!"
-    # else
-    #   new_vote = Vote.new(post_id: params[:vote][:post_id], user_id: current_user.id, value: params[:vote][:updown])
-    #   vote.save
-    #   redirect_to root_path, notice: "Vote created!"
-    # end
-  end
-
-
-  private
-  def vote_params
-    params.require(:vote).permit(:user_id, :post_id, :value)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160827225150) do
+ActiveRecord::Schema.define(version: 20160828182049) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "user_id"
@@ -39,11 +39,19 @@ ActiveRecord::Schema.define(version: 20160827225150) do
   end
 
   create_table "votes", force: :cascade do |t|
-    t.integer  "user_id"
+    t.string   "votable_type"
+    t.integer  "votable_id"
+    t.string   "voter_type"
+    t.integer  "voter_id"
+    t.boolean  "vote_flag"
+    t.string   "vote_scope"
+    t.integer  "vote_weight"
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer  "post_id"
-    t.integer  "value",      default: 0
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.integer  "user_id"
+    t.index ["votable_id", "votable_type", "vote_scope"], name: "index_votes_on_votable_id_and_votable_type_and_vote_scope"
+    t.index ["voter_id", "voter_type", "vote_scope"], name: "index_votes_on_voter_id_and_voter_type_and_vote_scope"
   end
 
 end
