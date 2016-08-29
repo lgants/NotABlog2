db1e472a97f801b421af57c0af366ffd758ab5e4
lgants <lgants@gmail.com>
2016-08-29 12:10:52 -0400

    fixed vote count mechanism

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -3,8 +3,16 @@ class VotesController < ApplicationController
 
   def create
     if vote = Vote.find_by(user_id: params[:vote][:user_id], post_id: params[:vote][:post_id])
-      vote.update(value: params[:vote][:value])
-      redirect_to root_path, notice: "Vote updated!"
+      if vote.value == -1 && params[:vote][:value].to_i == -1
+        vote.update(value: 0)
+        redirect_to root_path, notice: "Vote removed!"
+      elsif vote.value == 1 && params[:vote][:value].to_i == 1
+        vote.update(value: 0)
+        redirect_to root_path, notice: "Vote removed!"
+      else
+        vote.update(value: params[:vote][:value])
+        redirect_to root_path, notice: "Vote updated!"
+      end
     else
       vote = Vote.create(user_id: params[:vote][:user_id], post_id: params[:vote][:post_id], value: params[:vote][:value])
       redirect_to root_path, notice: "Vote created!"
