30b1d2817b0ec524f565de00c72096bbf5a3a1b4
colbyschulz <colbyschulz@gmail.com>
2016-08-28 22:19:20 -0400

    Added post image URL and post paperclip image functionality. Homepage now shows images if a image is added to post

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,6 @@ class Post < ApplicationRecord
   has_many :votes
   has_many :comments
   acts_as_votable
+  has_attached_file :post_image, styles: { medium: "300x300>", thumb: "50x50>" }
+  validates_attachment_content_type :post_image, content_type: /\Aimage\/.*\z/
 end
