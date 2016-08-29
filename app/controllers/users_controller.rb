11149e239b117886d8739ffed61c78b48ffc5d53
colbyschulz <colbyschulz@gmail.com>
2016-08-29 10:05:07 -0400

    conditional vote buttons and added styling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,19 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-
-  # GET /users
   def index
     @users = User.all
   end
 
-  # GET /users/1
   def show
   end
 
-  # GET /users/new
   def new
     @user = User.new
   end
 
-  # GET /users/1/edit
   def edit
   end
 
-  # POST /users
   def create
     @user = User.new(user_params)
     if @user.save
@@ -30,7 +24,6 @@ class UsersController < ApplicationController
     end
   end
 
-  # PATCH/PUT /users/1
   def update
     respond_to do |format|
       if @user.update(user_params)
@@ -43,7 +36,6 @@ class UsersController < ApplicationController
     end
   end
 
-  # DELETE /users/1
   def destroy
     @user.destroy
     respond_to do |format|
@@ -53,12 +45,11 @@ class UsersController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_user
       @user = User.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
       params.require(:user).permit(:first_name, :last_name, :username, :email, :password, :avatar)
     end
